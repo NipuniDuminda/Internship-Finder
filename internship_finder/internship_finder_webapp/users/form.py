e43185994e1efde51114374233709d43680d9c67
sandunsrimal <bandara-cs19005@stu.kln.ac.lk>
2024-02-17 20:26:05 +0530

    Delete previous project and add a new one

--- internship_finder/internship_finder_webapp/users/form.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.forms import UserChangeForm
-from django.contrib.auth import get_user_model
-
-class RegisterUserForm(UserChangeForm):
-    class Meta:
-        model = get_user_model()
-        fields = ['email', 'password', ]
